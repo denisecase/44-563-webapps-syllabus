5442985f4c377afb82cca49a4eb9235e140fb059
Denise Case <dcase@nwmissouri.edu>
2019-05-09 16:20:21 -0500

    F19

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 console.log("Service worker starting");
 
-const CACHE_NAME = '44-563-v1';
-const CACHE_CONTAINING_ERROR_MESSAGES = 'error-cache'
+const CACHE = '44-563-v1';
+const CACHE_CONTAINING_ERROR_MESSAGES = '44-563-error-cache'
 const urlsToCache = [
   '/images/N60-2Stack-Full.jpg',
   '/images/northwestlogo.jpg',
@@ -19,13 +19,13 @@ self.addEventListener('install', event => {
   console.log("Service worker install starting");
   // Perform install steps - open cache, cache files, confirm
   event.waitUntil(
-    caches.open(CACHE_NAME)
+    caches.open(CACHE)
       .then(cache => {
-        console.log('Opened cache ', CACHE_NAME);
+        console.log('Opened cache ', CACHE);
         return cache.addAll(urlsToCache);
       })
       .catch(err => {
-        console.log('Error opening cache ', CACHE_NAME);
+        console.log('Error opening cache ', CACHE);
         return;
       })
   );
@@ -47,26 +47,26 @@ self.addEventListener('fetch', event => {
   console.log("Service worker got a fetch request");
   event.respondWith(
     caches.match(event.request)
-      .then(function (response) {
-         if (response) { 
-          console.log("Service worker got a valid response from the cache");
-          return response; 
+      .then(response => {
+        if (response) {
+          console.log('Service worker found ', event.request.url, ' in the cache');
+          return response;
         }
         else {
           console.log("Service worker fetching updated content from the web");
-          return fetch(event.request)  
-            .then(function (res) {
+          return fetch(event.request)
+            .then(res => {
               console.log("Service worker saving response and returning fetched data")
-              return caches.open(CACHE_NAME)
-                .then(function (cache) {
-                  cache.put(event.request.url, res.clone());  
+              return caches.open(CACHE)
+              .then(cache => {
+                  cache.put(event.request.url, res.clone());
                   return res;
                 })
             })
-            .catch(function (err) {      
+            .catch(function (err) {
               console.log("Service worker fetch unsuccessful - replying with error page or message")
               return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                .then(function (cache) {
+              .then(cache => {
                   return cache.match('Error - not available');
                 });
             });
@@ -76,18 +76,3 @@ self.addEventListener('fetch', event => {
   console.log("Service worker finished fetch");
 });
 
-function fromCache(request) {
-  return caches.open(CACHE_NAME).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
-function update(request) {
-  return caches.open(CACHE_NAME).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
