0408bb862da1e9aea1dd2b208784460218898cfa
Denise Case <dcase@nwmissouri.edu>
2019-08-10 16:08:54 -0500

    merging prettier and eslint

--- service-worker.js
+++ service-worker.js
@@ -28,44 +28,44 @@
 
 // eslint-disable-next-line no-undef
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
+  'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'
+)
 
 if (workbox) {
-  console.log("Service worker Workbox loaded", workbox.routing);
+  console.log('Service worker Workbox loaded', workbox.routing)
 
-  const appName = "44-563-webapps-syllabus";
-  const appVersion = "v1";
-  const maxAgeDay = 1 * 24 * 60 * 60;
+  const appName = '44-563-webapps-syllabus'
+  const appVersion = 'v1'
+  const maxAgeDay = 1 * 24 * 60 * 60
   // eslint-disable-next-line no-unused-vars
-  const httpResponseOpaque = 0; // CORS
-  const httpReponseOk = 200; // good
+  const httpResponseOpaque = 0 // CORS
+  const httpReponseOk = 200 // good
 
   // test Regular Expressions at https://regexr.com/
-  const reStatic = /\.(?:js|css)$/;
-  const reImages = /\.(?:png|gif|jpg|jpeg|webp|svg)$/;
-  const reCdnFont = /https:\/\/use\.fontawesome\.com\/.*all\.css$/;
-  const reCdnStyles = /https:\/\/cdnjs\.cloudflare\.com\/.*\.css$/;
+  const reStatic = /\.(?:js|css)$/
+  const reImages = /\.(?:png|gif|jpg|jpeg|webp|svg)$/
+  const reCdnFont = /https:\/\/use\.fontawesome\.com\/.*all\.css$/
+  const reCdnStyles = /https:\/\/cdnjs\.cloudflare\.com\/.*\.css$/
 
   workbox.core.setCacheNameDetails({
     prefix: appName,
     suffix: appVersion,
-    precache: "custom-precache-name",
-    runtime: "custom-runtime-name"
-  });
+    precache: 'custom-precache-name',
+    runtime: 'custom-runtime-name'
+  })
 
-  const precacheCacheName = workbox.core.cacheNames.precache;
-  const runtimeCacheName = workbox.core.cacheNames.runtime;
+  const precacheCacheName = workbox.core.cacheNames.precache
+  const runtimeCacheName = workbox.core.cacheNames.runtime
 
-  console.log(`precacheCacheName=${precacheCacheName}`);
-  console.log(`runtimeCacheName=${runtimeCacheName}`);
+  console.log(`precacheCacheName=${precacheCacheName}`)
+  console.log(`runtimeCacheName=${runtimeCacheName}`)
 
   // use stale cached cdn font files while downloading new
 
   workbox.routing.registerRoute(
     reCdnFont,
     new workbox.strategies.StaleWhileRevalidate()
-  );
+  )
 
   // use stale cached cdn style files while downloading new
 
@@ -84,7 +84,7 @@ if (workbox) {
         })
       ]
     })
-  );
+  )
 
   // Use stale local static files (js/css) while downloading new
 
@@ -100,7 +100,7 @@ if (workbox) {
         })
       ]
     })
-  );
+  )
 
   // Fetch images, try local cache first
 
@@ -116,36 +116,36 @@ if (workbox) {
         })
       ]
     })
-  );
+  )
 
   // Define a common handler if any of the fetching methods fail
 
   workbox.routing.setCatchHandler(({ event }) => {
-    console.error(`Error: ${event.error}`);
-    if (event.request.mode === "navigate") {
-      return caches.match("/error-page.html");
+    console.error(`Error: ${event.error}`)
+    if (event.request.mode === 'navigate') {
+      return caches.match('/error-page.html')
     }
-    return Response.error();
-  });
+    return Response.error()
+  })
 
   // respond with 200 (ok) even when offline
 
-  self.addEventListener("install", function(event) {
+  self.addEventListener('install', event => {
     event.waitUntil(
       caches.open(`${appName}-static`).then(cache => {
-        return cache.addAll([".", "index.html", "styles/case-syllabus.css"]);
+        return cache.addAll(['.', 'index.html', 'styles/case-syllabus.css'])
       })
-    );
-  });
+    )
+  })
 
-  self.addEventListener("fetch", event => {
+  self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request).then(response => {
         if (response) {
-          return response;
+          return response
         }
-        return fetch(event.request);
+        return fetch(event.request)
       })
-    );
-  });
+    )
+  })
 }
